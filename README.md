1c412cbb79bc068c7cbfcd43ebc3ba971ef727ab
mrasamny <mrasamny@desu.edu>
2021-02-10 10:41:22 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# Painting a Wall
+
+1. Prompt the user to input a wall's height and width. Calculate and output the wall's area. (2 pts)
+
+```
+Enter wall height (feet):
+12
+Enter wall width (feet):
+15
+Wall area: 180 square feet
+```
+
+2. Extend to also calculate and output the amount of paint in gallons needed to paint the wall. Assume a gallon of paint covers 350 square feet. Store this value using a const double variable. (2 pts)
+
+```
+Enter wall height (feet):
+12
+Enter wall width (feet):
+15
+Wall area: 180 square feet
+Paint needed: 0.5142857142857142 gallons
+```
+
+3. Extend to also calculate and output the number of 1 gallon cans needed to paint the wall. Hint: Use a math function to round up to the nearest gallon. (2 pts)
+
+```
+Enter wall height (feet):
+12
+Enter wall width (feet):
+15
+Wall area: 180 square feet
+Paint needed: 0.5142857142857142 gallons
+Cans needed: 1 can(s)
+```
---
+++ src/main/java/SkillBuilder1b.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+/**
+ * Skill Builder 3
+ *
+ * @author (You)
+ * @version (2.1, 2.2)
+ */
+public class SkillBuilder1b {
+
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        double wallHeight = 0.0;
+        double wallWidth = 0.0;
+        double wallArea = 0.0;
+        double gallonsPaintNeeded = 0.0;
+        int cansNeeded = 0;
+
+        final double squareFeetPerGallons = 350.0;
+        final double gallonsPerCan = 1.0;
+
+        // Prompt user to input wall's width
+        System.out.println("Enter wall height (feet):");
+        wallHeight = scnr.nextDouble();
+
+        // Prompt user to input wall's width
+        System.out.println("Enter wall width (feet):");
+        wallWidth = scnr.nextDouble();
+
+        // TODO: Calculate and output the wall's area
+
+        // TODO: Calculate and output the amount of paint in gallons needed to paint the wall
+
+        // TODO: Calculate and output the number of 1 gallon cans needed to paint the wall, rounded up to nearest integer
+
+    }
+}
